6e814d517711eb4b8990c53be7ad96fdbe44a354
nervenet450-hub <nervenet450@gmail.com>
2026-04-17 22:05:04 +0200

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  devIndicators: {
+    buildActivity: false,
+    buildActivityPosition: 'bottom-left',
+  },
+};
+
+export default nextConfig;
